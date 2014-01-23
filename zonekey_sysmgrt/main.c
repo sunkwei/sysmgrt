f987a4985b25e2e9061d09f073fb5982320bff05
sunkw <sunkwei@gmail.com>
2014-01-23 12:09:27 +0800

    prepared for zonekey system managerment, based WebService

---
+++ zonekey_sysmgrt/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  zonekey_sysmgrt
+//
+//  Created by sunkw on 14-1-23.
+//  Copyright (c) 2014年 sunkw. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
