4f0a3e7edb876180ea9df7d95e552eab09b98d81
sunkw <sunkwei@gmail.com>
2014-02-06 14:06:42 +0800

    ..

--- sysmgrt.net/sysmgrt.net/Program.cs
+++ sysmgrt.net/sysmgrt.net/Program.cs
@@ -10,26 +10,27 @@ namespace sysmgrt.net
 			zkreg reg = new zkreg ();
 			reg.Url = "http://localhost:8899";
 
-			Host host = new Host ();
-			host.name = Guid.NewGuid ().ToString ("D");
-			host.catalog = Catalog.Host;
-			host.ips = new string[1];
-			host.ips [0] = "192.168.1.10";
-			host.showname = "汉字";
+			for (int i = 0; i < 1000; i++) {
 
+				Host host = new Host ();
+				host.name = Guid.NewGuid ().ToString ("D");
+				host.catalog = Catalog.Host;
+				host.ips = new string[1];
+				host.ips [0] = "192.168.1.10";
+				host.showname = "汉字";
 
+				MessageRegHost mreg = new MessageRegHost ();
+				mreg.regHostReq = host;
 
-			MessageRegHost mreg = new MessageRegHost ();
-			mreg.regHostReq = host;
+				MessageRegHostResponse mres = reg.regHost (mreg);
 
-			MessageRegHostResponse mres = reg.regHost (mreg);
-
-			Console.WriteLine ("token=" + mres.token);
+				Console.WriteLine ("token=" + mres.token);
+			}
 		}
 
 		public static void Main (string[] args)
 		{
-			int count = 1;
+			int count = 10;
 			Thread []th = new Thread[count];
 
 			for (int i = 0; i < count; i++) {
